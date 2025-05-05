09eab5bcc1327e28d522903a83549a8542f7fbbb
Khatibullah Rahel <khatibullahrahel25@gmail.com>
2025-05-05 18:08:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+export const systemMsg = {
+  role: "system",
+  content: `
+  your name is Taskly! you are a task advisor!
+  `,
+};
+
+export const getAdvice = async () => {
+  const reply = await puter.ai.chat(systemMsg, {
+    model: "claude-3-7-sonnet",
+  });
+
+  return reply;
+};
